5404fde21aabe51cb9a1b48269e75f889b65b19e
MixterOne <matheusisaias879@gmail.com>
2022-01-17 21:24:29 -0300

    confirmar

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const produtos = [
         id: 0,
         nome: 'Galaxy A02',
         img: '../imgs/GalaxyA02.png',
+        desc: 'loremmmmmmm',
+        preço: 'R$-500',
         quantidade: 0
     },
 
@@ -10,6 +12,8 @@ const produtos = [
         id: 1,
         nome: 'Galaxy A024',
         img: '../imgs/GalaxyA02.png',
+        desc: 'loremmmmmmm',
+        preço: 'R$-300',
         quantidade: 0
     },
 
@@ -17,10 +21,39 @@ const produtos = [
         id: 2,
         nome: 'Galaxy A023',
         img: '../imgs/GalaxyA02.png',
+        desc: 'loremmmmmmm',
+        preço: 'R$-200',
         quantidade: 0
     }
 ]
 
 iniciarLoja = () =>{
-    
+ 
+
+    let produtosContainer = document.querySelector('#produtos');
+
+    for(let produto of produtos){
+        produtosContainer.innerHTML += `
+        <div class="card">
+    <div class="imgBx">
+      <img src= ${produto.img}>
+    </div>
+    <div class="contentBx">
+      <h2>${produto.nome}}</h2>
+      
+      <div class="color">
+        <p>${produto.desc}</p>
+      </div>
+      <div class = "preco">
+        <h3>${produto.preço}</h3>
+      </div>
+
+      <a href="#">Buy Now</a>
+    </div>
+  </div>
+
+        `;
+    }
 }
+
+iniciarLoja()
